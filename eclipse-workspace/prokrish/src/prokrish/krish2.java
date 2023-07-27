90dd1085a30b580258fdd0a0910ac6ae660d08dd
shrik <shrikrishna.dound@gmail.com>
2023-07-27 18:44:55 +0530

    this is krish2

---
+++ eclipse-workspace/prokrish/src/prokrish/krish2.java
@@ -0,0 +1,10 @@
+package prokrish;
+
+public class krish2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("this is krish 2");
+	}
+
+}
